0d1900980443c0ac9089b9de014db988b1dd927a
Ivan Krukov <ivan.o.kryukov@gmail.com>
2020-06-17 13:03:52 -0400

    down with the discrete flag

--- scripts/sim_continue.py
+++ scripts/sim_continue.py
@@ -16,7 +16,7 @@ ts = msprime.load(args.input_ts)
 # read the recombination map from the provenance
 provenance = json.loads(ts.provenance(0).record)
 ts_recomb_map = provenance["parameters"]["recombination_map"]
-rm = msprime.RecombinationMap(ts_recomb_map["positions"], ts_recomb_map["rates"], discrete=ts_recomb_map["discrete"])
+rm = msprime.RecombinationMap(ts_recomb_map["positions"], ts_recomb_map["rates"])
 
 sim = msprime.simulate(from_ts = ts, model='dtwf', Ne=args.population_size, recombination_map=rm)
 
