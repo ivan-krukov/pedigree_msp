f247533051abde10f26f2375b3387908ad8284de
Ivan Krukov <ivan.o.kryukov@gmail.com>
2020-06-17 12:03:40 -0400

    reading recomb map from provenance

--- scripts/sim_continue.py
+++ scripts/sim_continue.py
@@ -1,6 +1,7 @@
 import msprime
 import sys
 from argparse import ArgumentParser
+import json
 
 parser = ArgumentParser('sim_continue')
 
@@ -12,6 +13,11 @@ args = parser.parse_args()
 
 ts = msprime.load(args.input_ts)
 
-sim = msprime.simulate(from_ts = ts, model='dtwf', Ne=args.population_size)
+# read the recombination map from the provenance
+provenance = json.loads(ts.provenance(0).record)
+ts_recomb_map = provenance["parameters"]["recombination_map"]
+rm = msprime.RecombinationMap(ts_recomb_map["positions"], ts_recomb_map["rates"], discrete=ts_recomb_map["discrete"])
+
+sim = msprime.simulate(from_ts = ts, model='dtwf', Ne=args.population_size, recombination_map=rm)
 
 sim.dump(args.output_ts)
